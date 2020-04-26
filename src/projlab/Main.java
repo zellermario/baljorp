cb50dc74d44694053133d41c2277202ac0c272df
Mario Zeller <zellermario@me.com>
2020-04-26 18:14:21 +0200

    Main + TextInterface bemenet kész
    Még van néhány TODO + a szöveges kimenet még nincsen kész, csak a bemenet.

--- src/projlab/Main.java
+++ src/projlab/Main.java
@@ -1,8 +1,19 @@
 package projlab;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String args[]) {
+		System.out.print("$ ");
+		Jatek jatek = new Jatek();
+		TextInterface jatekIF = new TextInterface(jatek);
+		Scanner console = new Scanner(System.in);
+		while (console.hasNextLine()) {
+			jatekIF.executeCommand(console.nextLine());
+			System.out.print("$ ");
+		}
+		console.close();
 	}
 	
 }
